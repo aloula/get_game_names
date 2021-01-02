fab24566a6a8ff345c21d79ba07b0ba8aa3f2e64
aloula <alex.loula@gmail.com>
2021-01-02 16:14:44 -0300

    changed output folders

--- build.sh
+++ build.sh
@@ -2,17 +2,15 @@
 ARM_VERSION=6
 ARCH_RPI=arm
 ARCH_DESKTOP=amd64
-PROGRAM_NAME_RPI='getgamenames_rpi'
-PROGRAM_NAME_LINUX='getgamenames_linux_x64'
-PROGRAM_NAME_WINDOWS='getgamenames_win_x64'
+PROGRAM_NAME_RPI='getgamenames'
  
 echo "Building for Linux ARM (RPI)..."
-env GOOS=linux GOARCH=$ARCH_RPI GOARM=$ARM_VERSION go build -o ./builds/$PROGRAM_NAME_RPI getgamenames.go
+env GOOS=linux GOARCH=$ARCH_RPI GOARM=$ARM_VERSION go build -o ./builds/rpi/$PROGRAM_NAME_RPI getgamenames.go
 
 echo "Building for Linux x64..."
-env GOOS=linux GOARCH=$ARCH_DESKTOP go build -o ./builds/$PROGRAM_NAME_LINUX getgamenames.go
+env GOOS=linux GOARCH=$ARCH_DESKTOP go build -o ./builds/linux_x64/$PROGRAM_NAME_LINUX getgamenames.go
 
 echo "Building for Windows x64..."
-env GOOS=windows GOARCH=$ARCH_DESKTOP go build -o ./builds/$PROGRAM_NAME_WINDOWS getgamenames.go
+env GOOS=windows GOARCH=$ARCH_DESKTOP go build -o ./builds/win_x64/$PROGRAM_NAME_WINDOWS getgamenames.go
 
 exit 0 
